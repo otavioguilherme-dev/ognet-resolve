93f161742c96d7c4d1394986aec2095a52f39305
otavioguilherme-dev <otavioguilherme@gmail.com>
2026-03-09 21:07:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -90,10 +90,11 @@ with col1:
     st.write("Ainda precisa de ajuda?")
 with col2:
     # Substitua pelo seu número real do WhatsApp
-    link_whatsapp = "https://wa.me/5511994251306?text=OGNET BORRACHAS me ajuda a instalar a minha borracha!!!!!"
+    link_whatsapp = "https://wa.me/5511994251306?text=OGNET BORRACHAS me ajude a instalar a minha borracha!!!!!"
     st.link_button("Falar com Técnico no WhatsApp", link_whatsapp)
 
 st.caption("OGNET-BORRACHAS Resolve - Facilitando sua instalação.")
+
 
 
 
